64d81c95190da7123f8adcdaea6fca88776b80ad
Chris Johnson <chrisjohnson00@hotmail.com>
2023-03-18 23:15:32 -0700

    feat: Initial release (#1)
    * feat: initial version

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # sonarr-daily-show-cleanup
 A script which keeps up the last N of any 'daily' episode shows in your library
+
+## PyPi Dependencies
+
+``` 
+docker run -it --rm -v ${PWD}:/repo -w /repo python:3.11.2-slim bash
+pip install --upgrade pip
+pip install --upgrade pygogo requests
+pip freeze > requirements.txt
+sed -i '/pkg_resources/d' requirements.txt
+```
+
+## Running
+
+```commandline
+docker build . -t chrisjohnson00/sonarr-daily-show-cleanup
+
+docker run --rm -e SONARR_HOST=http://192.168.1.131:8989 \
+    -e SONARR_APIKEY=xxxx \
+    -v /mnt/video/Television:/tv \
+    --user 1000:1000 \
+    chrisjohnson00/sonarr-daily-show-cleanup
+```
---
+++ app.py
@@ -0,0 +1,81 @@
+import requests
+import re
+import os
+import pygogo as gogo
+
+# logging setup
+kwargs = {}
+formatter = gogo.formatters.structured_formatter
+logger = gogo.Gogo('struct', low_formatter=formatter).get_logger(**kwargs)
+
+
+def main():
+    logger.info("Starting")
+    # Set up variables for Sonarr API
+    sonarr_url = get_config('SONARR_HOST')
+    sonarr_api_key = get_config('SONARR_APIKEY')
+    # headers = {'X-Api-Key': sonarr_api_key}
+
+    # Get list of shows that are set to air daily
+    sonarr_daily_shows = []
+    response = requests.get(f'{sonarr_url}/api/series?apikey={sonarr_api_key}')
+    json_data = response.json()
+
+    for show in json_data:
+        if show['seriesType'] == 'daily':
+            sonarr_daily_shows.append(show)
+
+    logger.debug('Found the following daily shows', extra={'shows': sonarr_daily_shows})
+
+    # For each daily show, get list of episode directories and delete all but the last 7
+    for show in sonarr_daily_shows:
+        show_id = show['id']
+        show_title = show['title']
+        show_files = []
+        url = f'{sonarr_url}/api/v3/episode?seriesId={show_id}&includeImages=false'
+        logger.info(f'Getting episodes for seriesId {show_id}', extra={'url': url})
+        response = requests.get(f'{url}&apikey={sonarr_api_key}')
+        json_data = response.json()
+
+        for episode in json_data:
+            if episode['hasFile']:
+                episode_id = episode['id']
+                url = f'{sonarr_url}/api/v3/episode/{episode_id}'
+                logger.info(f'Getting episode {episode_id}', extra={'url': url})
+                response = requests.get(f'{url}?apikey={sonarr_api_key}')
+                episode_data = response.json()
+                show_files.append(episode_data['episodeFile']['path'])
+
+        logger.debug(f'Found files for {episode_id}', extra={'files': show_files})
+        files_to_delete = get_old_files(show_files, 5)
+
+        logger.debug(f'Will delete the following files for {episode_id}', extra={'files_to_delete': files_to_delete})
+        # Delete files
+        for file_path in files_to_delete:
+            if os.path.isfile(file_path):
+                logger.info("Deleting file", extra={'file_path': file_path})
+                # os.remove(file_path)
+
+        logger.info(f'{show_title}: {len(files_to_delete)} episodes deleted')
+
+
+def get_config(key):
+    if os.environ.get(key):
+        return os.environ.get(key)
+
+
+def get_old_files(file_list, older_than_x_days):
+    # Define regular expression pattern to find date strings in file names
+    date_pattern = r"\d{4}-\d{2}-\d{2}"
+
+    # Sort files by date
+    file_list.sort(key=lambda x: re.search(date_pattern, x).group(0))
+
+    # Get all files except the latest older_than_x_days
+    old_files = file_list[:-older_than_x_days]
+
+    return old_files
+
+
+if __name__ == '__main__':
+    main()
